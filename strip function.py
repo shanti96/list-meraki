875d9cdbe7def09d188de9cb782f32e697581e7c
shanti96 <106430039+shanti96@users.noreply.github.com>
2022-08-05 08:05:01 +0530

    Add files via upload

---
+++ strip function.py
@@ -0,0 +1,14 @@
+# str1 = "   Hello World   "
+# print(str1.strip()) 
+# print(str1.strip("He"))
+
+# str2 ="Hii Rakesh Hii"
+# print(str2.strip("ra kesh")) 
+# print(str2.strip("Hi"))     
+
+str="helo sushreeh"  
+print(len(str))
+print(str)
+s=str.strip('h') 
+print(s)
+print(len(s)) 
